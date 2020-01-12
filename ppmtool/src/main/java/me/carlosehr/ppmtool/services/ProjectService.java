a243c5ddd2a5795a5c0ea7f5a4716aa8c5293cab
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-11 17:18:57 -0800

    Exceptions for unique Project identifiers

--- ppmtool/src/main/java/me/carlosehr/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/me/carlosehr/ppmtool/services/ProjectService.java
@@ -2,18 +2,24 @@ package me.carlosehr.ppmtool.services;
 
 import me.carlosehr.ppmtool.ProjectRepository;
 import me.carlosehr.ppmtool.domain.Project;
+import me.carlosehr.ppmtool.exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
+
     @Autowired
     private ProjectRepository projectRepository;
 
+
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+ "' already exists");
+        }
     }
 }
