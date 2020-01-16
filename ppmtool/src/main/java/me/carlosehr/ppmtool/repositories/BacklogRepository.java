ed3432329bdcb35ad18fa67f679aafa3c29cb984
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-16 00:13:10 -0800

    Created entities Backlog and ProjectTask

---
+++ ppmtool/src/main/java/me/carlosehr/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,10 @@
+package me.carlosehr.ppmtool.repositories;
+
+import me.carlosehr.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+}
