53f7fc4e3caa6c005353b3bf2b00b54927b30527
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-17 15:07:19 -0800

    Add project task component added

--- ppmtool-react-client/src/reducers/backlogReducer.js
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -1,32 +1,36 @@
-GET_BACKLOG,  GET_PROJECT_TASK,  DELETE_PROJECT_TASK}  from "../actions/types";
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from "../actions/types";
 
 const initialState = {
-    project_tasks: [],
-    project_task: {}
-}
+  project_tasks: [],
+  project_task: {}
+};
 
-export default function(state = initialState, action){
-    switch(action.type){
-        
-        case GET_BACKLOG: return{
-            ...state,
-            project_tasks: action.payload
-        }
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        project_tasks: action.payload
+      };
 
-        case GET_PROJECT_TASK:
-            return{
-                ...state,
-                project_task:action.payload
-            }
-        
-        case DELETE_PROJECT_TASK:
-            return {
-                ...state
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload
+      };
 
-                //TO_DO
-            }
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state
 
-        default:
-            return state;
-    }
+        //TO_DO
+      };
+
+    default:
+      return state;
+  }
 }
