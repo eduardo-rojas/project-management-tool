8c0d34cffd4fb1a55a102819f10b2abc0289f74b
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-18 02:34:51 -0800

    Project Tasks loaded from State to UI

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -15,6 +15,7 @@ class ProjectBoard extends Component {
 
   render() {
     const { id } = this.props.match.params;
+    const { project_tasks } = this.props.backlog;
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -22,7 +23,7 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
-        <Backlog />
+        <Backlog project_tasks_prop={project_tasks} />
       </div>
     );
   }
@@ -37,4 +38,4 @@ const mapStateToProps = state => ({
   backlog: state.backlog
 });
 
-export default connect(null, { getBacklog })(ProjectBoard);
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
